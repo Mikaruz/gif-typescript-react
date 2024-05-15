657daa5ff21e82a1fbe070ad4cd01842b9b6146c
Mikaruz <gersonsebas2019@gmail.com>
2024-05-14 23:30:27 -0500

    Funcionalid mejorada

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,10 @@ import { GifInput } from "./components/GifInput";
 import { GifGrid } from "./components/GifGrid";
 
 function App() {
-  const [categories, setCategories] = useState<string[]>([]);
+  const [category, setCategory] = useState<string>("");
 
   const onAddCategory = (newCategory: string) => {
-    if (categories.includes(newCategory)) return;
-    setCategories([newCategory, ...categories]);
+    setCategory(newCategory);
   };
 
   return (
@@ -15,9 +14,7 @@ function App() {
       <h1>GifExpertApp</h1>
       <GifInput onNewCategory={onAddCategory} />
 
-      {categories.map((category) => (
-        <GifGrid key={category} category={category} />
-      ))}
+      {category && <GifGrid category={category} />}
     </>
   );
 }
--- src/helpers/getGifs.ts
+++ src/helpers/getGifs.ts
@@ -1,5 +1,7 @@
 export const getGifs = async (category: string) => {
-  const apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=Mew7NdnfnP1wIFNLlzBGjCvwGP80cIOu&q=${category}&limit=100`;
+  const apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${
+    import.meta.env.VITE_API_KEY
+  }&q=${category}&limit=2`;
 
   const response = await fetch(apiUrl);
   const { data } = await response.json();
